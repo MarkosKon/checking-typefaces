bb2512132a1c121a0c6054aa4d9518ee3ae7a3bc
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-01-31 20:03:12 +0200

    Added an example with IBM Plex serif.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 import "@affectionatedoor/opentype"
-import "./src/styles/ibm-plex-sans.css"
+import "./src/styles/ibm-plex-serif.css"
 import "./src/styles/ibm-plex-mono.css"
 import "./src/styles/main.css"
