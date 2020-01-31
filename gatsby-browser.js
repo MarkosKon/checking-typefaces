0440338b5e1f51423abfdcc12bcbecaea4b53b8d
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-01-31 20:40:52 +0200

    Added an example that uses the sans-serif version for roman, greek and greek italic, and the serif version for italic.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 import "@affectionatedoor/opentype"
-import "./src/styles/ibm-plex-serif.css"
+import "./src/styles/ibm-plex.css"
 import "./src/styles/ibm-plex-mono.css"
 import "./src/styles/main.css"
