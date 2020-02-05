f84ef07b418495bbd2a848f1568582d133f4a568
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-02-05 23:55:22 +0200

    Added an example for Lato.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,3 @@
 import "@affectionatedoor/opentype"
-import "@affectionatedoor/source-sans-subsets"
-// import "@affectionatedoor/source-sans-subsets/variable.css"
+import "./src/styles/lato.css"
 import "./src/styles/main.css"
