0a62b0991fdf2297d4cc92b69b9d783ad9e49ae5
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-10-08 13:37:04 +0300

    Remove hinting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,4 @@
 import "@affectionatedoor/opentype"
-import "./src/styles/gfs-neohellenic.css"
+// import "./src/styles/gfs-neohellenic.css"
+import "./src/styles/gfs-neohellenic-no-hinting.css"
 import "./src/styles/main.css"
